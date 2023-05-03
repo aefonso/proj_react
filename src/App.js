2485884db412e8771526f85564aab98a3102d273
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-05-03 17:54:24 -0300

    feat: montando estrutura do projeto e configurando as rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 function App() {
   return (
-    <div className="App">
-        <p>
-         Projeto Teste
-        </p>
-    </div>
+    <Router>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+          
+        </ul>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route  path='/company' element={<Company/>}/>
+          <Route  path='/contact' element={<Contact/>}/>
+          <Route  path='/newproject' element={<NewProject/>}/>   
+        </Routes>
+        <p>Footer</p>
+    </Router>
   );
 }
 
