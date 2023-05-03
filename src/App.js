9824801c9fc2e5d9aec514faca8f6d58e04397a4
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-05-03 18:32:58 -0300

    feat: update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 function App() {
   return (
     <Router>
-        <ul>
+       <Container customClass="min-height">
+        <div>
           <Link to="/">Home</Link>
           <Link to="/contact">Contato</Link>
           <Link to="/company">Empresa</Link>
-          <Link to="/newproject">Novo Projeto</Link>
-          
-        </ul>
-        <Routes>
+          <Link to="/newproject">Novo Projeto</Link>  
+        </div>
+        <Routes>  
           <Route exact path='/' element={<Home/>}/>
           <Route  path='/company' element={<Company/>}/>
           <Route  path='/contact' element={<Contact/>}/>
-          <Route  path='/newproject' element={<NewProject/>}/>   
+          <Route  path='/newproject' element={<NewProject/>}/>
         </Routes>
         <p>Footer</p>
+        </Container>  
     </Router>
   );
 }
