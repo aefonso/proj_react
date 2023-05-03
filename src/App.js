3b8c8064d390ae9d88f3f2ddc8ef07497be5a9bd
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-05-03 19:52:57 -0300

    Criando novas páginas/componentes e configurando css

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 function App() {
   return (
     <Router>
-       <Container customClass="min-height">
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contato</Link>
-          <Link to="/company">Empresa</Link>
-          <Link to="/newproject">Novo Projeto</Link>  
-        </div>
+       
+        <Navbar />
+        <Container customClass="min-height">
         <Routes>  
           <Route exact path='/' element={<Home/>}/>
-          <Route  path='/company' element={<Company/>}/>
+          <Route  path='/projects' element={<Projects/>}/>
           <Route  path='/contact' element={<Contact/>}/>
           <Route  path='/newproject' element={<NewProject/>}/>
         </Routes>
-        <p>Footer</p>
-        </Container>  
+        </Container> 
+        <Footer/> 
     </Router>
   );
 }
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,5 @@
+function Projects () {
+  return <h1>Home</h1>
+}
+
+export default Projects;
