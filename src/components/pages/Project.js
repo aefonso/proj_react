9b0f742f59e5b7ead3b928211837c674e0c5ab8e
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-05-25 14:06:46 -0300

    feat:update

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,12 +2,14 @@ import styles from './Project.module.css'
 
 import {useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 
 function Project() {
   const { id } = useParams()
   
   const [project, setProject] = useState([])
-
+  const [showProjectForm, setShowProjectForm] = useState(false)
   useEffect (() => {
     fetch(`http://localhost:5000/projects/${id}`, {
     method: 'GET',
@@ -21,8 +23,25 @@ function Project() {
     })
     .catch((err) => console.log(err))
   }, [id])
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm)
+  }
   return (
-    <p>{project.name}</p>
+    <>
+    {project.name ? (
+    <div>
+      <Container customClass="column">
+        <div>
+          <h1>Projeto: {project.name}</h1>
+          <button onClick={toggleProjectForm}>
+            {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+          </button>
+        </div>
+      </Container>
+    </div>  
+   ) : (
+   <Loading />)}</>
   )
 }
 
